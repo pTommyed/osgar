d62bd30145a0607a86e6d1ab599c0e2ff75ef4d1
Martin Dlouhy <md@robotika.cz>
2019-05-10 07:32:48 +0200

    integrate Lord IMU

--- osgar/drivers/lord_imu.py
+++ osgar/drivers/lord_imu.py
@@ -3,6 +3,9 @@
 """
 # www.microstrain.com/sites/default/files/3dm-gx5-25_dcp_manual_8500-0065.pdf
 import struct
+import math
+
+from osgar.node import Node
 
 
 def verify_checksum(packet):
@@ -20,12 +23,15 @@ def verify_checksum(packet):
 def get_packet(buf):
     try:
         i = buf.index(b'\x75\x65')
-        packet_size = buf[i + 3] + 4 + 2  # + header size + checksum
-        packet = buf[i:i + packet_size]
-        verify_checksum(packet)
-        return packet, buf[i + packet_size:]
     except ValueError:
-        return b'', buf
+        return None, buf
+    packet_size = buf[i + 3] + 4 + 2  # + header size + checksum
+    if i + packet_size > len(buf):
+        return None, buf
+    packet = buf[i:i + packet_size]
+    verify_checksum(packet)
+    return packet, buf[i + packet_size:]
+
 
 def parse_packet(packet, verbose=False):
     assert packet.startswith(b'\x75\x65'), packet.hex()
@@ -35,36 +41,86 @@ def parse_packet(packet, verbose=False):
     desc = packet[2]
 
     # IMU Data Set (0x80)
-    assert desc == 0x80, hex(desc)
+    assert desc in [0x80, 0x82], hex(desc)
     i = 4
     acc, gyro, mag = None, None, None
     while i < packet_size - 2:
         field_length = packet[i]
         cmd = packet[i + 1]
 
-        if cmd == 0x04:
+        if desc == 0x80 and cmd == 0x04:
             # Scaled Accelerometer Vector
             assert field_length == 14, field_length
             acc = struct.unpack_from('>fff', packet, i + 2)
             if verbose:
                 print('acc', acc)
-        elif cmd == 0x05:
+        elif desc == 0x80 and cmd == 0x05:
             # Scaled Gyro Vector (0x80, 0x05)
             assert field_length == 14, field_length
             gyro = struct.unpack_from('>fff', packet, i + 2)
             if verbose:
                 print('gyro', gyro)
-        elif cmd == 0x06:
+        elif desc == 0x80 and cmd == 0x06:
             # Scaled Magnetometer Vector (0x80, 0x06)
             assert field_length == 14, field_length
             mag = struct.unpack_from('>fff', packet, i + 2)
             if verbose:
                 print('mag', mag)
+        elif desc == 0x82 and cmd == 0x03:
+            # Orientation, Quaternion (0x82, 0x03)
+            q0, q1, q2, q3, valid = struct.unpack_from('>ffffH', packet, i + 2)
+            if verbose:
+                print('quaterion', q0, q1, q2, q3, valid)
+        elif desc == 0x82 and cmd == 0x0A:
+            # Attitude Uncertainty, Euler Angles (0x82, 0x0A)
+            roll, pitch, yaw, valid = struct.unpack_from('>fffH', packet, i + 2)
+            if verbose:
+                print('euler', roll, pitch, yaw, valid)
+        elif desc == 0x82 and cmd == 0x0D:
+            # Linear Acceleration (0x82, 0x0D)
+            # Filter-Compensated Linear Acceleration Data (gravity vector removed)
+            x, y, z, valid = struct.unpack_from('>fffH', packet, i + 2)
+            if verbose:
+                print('lin', x, y, z, valid)
+        elif desc == 0x82 and cmd == 0x0E:
+            # Compensated Angular Rate (0x82, 0x0E)
+            x, y, z, valid = struct.unpack_from('>fffH', packet, i + 2)
+            if verbose:
+                print('ang', x, y, z, valid)
+        elif desc == 0x82 and cmd == 0x14:
+            # Heading Update Source information expressed in the sensor frame.
+            heading, uncertainty, source, valid = struct.unpack_from('>ffHH', packet, i + 2)
+            if verbose:
+                print('heading', heading, uncertainty, source, valid)
         else:
-            assert False, hex(cmd)
+            assert False, (hex(desc), hex(cmd))
 
         i += field_length
 
-        return acc, gyro, mag
+    return acc, gyro, mag
 
-# vim: expandtab sw=4 ts=4
+
+class LordIMU(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        self._buf = b''
+        self.raw = None  # not automatically defined yet
+
+    def update(self):
+        channel = super().update()
+        assert channel == 'raw', channel
+        self._buf += self.raw 
+        while True:
+            packet, self._buf = get_packet(self._buf)
+            if packet is None:
+                break
+            acc, gyro, mag = parse_packet(packet)
+            if mag is not None:
+                yaw, pitch, roll = mag
+                # CF Euler Angles (0x80, 0x0C)
+                # Pitch, Roll, and Yaw (aircraft) values
+                # This is a three component vector containing the Roll, Pitch and Yaw angles 
+                # in radians.
+                self.publish('rotation', [int(round(math.degrees(x) * 100)) for x in [yaw, pitch, roll]])
+        
+# vim: expandtab sw=4 ts=43DM-GX5-25
--- osgar/drivers/test_lord_imu.py
+++ osgar/drivers/test_lord_imu.py
@@ -1,16 +1,19 @@
 import unittest
 from unittest.mock import MagicMock
 
-from osgar.drivers.lord_imu import get_packet, verify_checksum, parse_packet
+from osgar.drivers.lord_imu import get_packet, verify_checksum, parse_packet, LordIMU
 from osgar.bus import BusHandler
+
+
+SAMPLE_DATA = b'\x0e\x06=\xea\x01\x19>\xc3R\xd2>\xdb?V\x1e\x93ue\x80*\x0e\x04<\x87C\x97\xbdF%\xd4\xbf\x7f\xb4\x14\x0e\x05\xba.\xd6S\xba)b\xeb\xb9\xf6\xf7\x80\x0e\x06=\xeaD\x10>\xc3\\\xd5>\xdb4\x1d\xda\x04ue\x80*\x0e\x04<\x9d\x1c\xa3\xbdS\x19\xd9\xbf\x7f\xbc^\x0e\x05\xbas\xd7W\xb9\x15\x03\xa0\xbaD\xffc\x0e\x06=\xea'
 
 
 class LordIMUTest(unittest.TestCase):
 
     def test_parse_packet(self):
-        self.assertEqual(get_packet(b''), (b'', b''))
+        self.assertEqual(get_packet(b''), (None, b''))
 
-        buf = b'\x0e\x06=\xea\x01\x19>\xc3R\xd2>\xdb?V\x1e\x93ue\x80*\x0e\x04<\x87C\x97\xbdF%\xd4\xbf\x7f\xb4\x14\x0e\x05\xba.\xd6S\xba)b\xeb\xb9\xf6\xf7\x80\x0e\x06=\xeaD\x10>\xc3\\\xd5>\xdb4\x1d\xda\x04ue\x80*\x0e\x04<\x9d\x1c\xa3\xbdS\x19\xd9\xbf\x7f\xbc^\x0e\x05\xbas\xd7W\xb9\x15\x03\xa0\xbaD\xffc\x0e\x06=\xea'
+        buf = SAMPLE_DATA
         packet, buf = get_packet(buf)
         self.assertTrue(packet.startswith(b'\x75\x65'), packet)
 #        print(packet.hex())
@@ -28,5 +31,22 @@ class LordIMUTest(unittest.TestCase):
         #replay: 7565 0104 04F1 0100 D56A
         # set to idle: 7565 0102 0202 E1C7
         # ping         7565 0102 0201 E0C6
+
+    def test_node(self):
+        config = {}
+#        q = MagicMock()
+        logger = MagicMock()
+        robot_bus = BusHandler(logger, out={}, name='robot')
+        bus = BusHandler(logger,
+                         out={'orientation':[(robot_bus.queue, 'orientation')], 'rotation':[]},
+                         name='imu')
+        imu = LordIMU(config, bus=bus)
+        imu.start()
+        imu.bus.queue.put((123, 'raw', SAMPLE_DATA))
+        imu.request_stop()
+        imu.join()
+        self.assertEqual(imu.raw, SAMPLE_DATA)
+#        q.put.assert_called_once_with((135, 'raw', 
+#            b'\x00\x00\x03\x01\x01\xfb'))
 
 # vim: expandtab sw=4 ts=4
